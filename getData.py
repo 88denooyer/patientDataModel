1d6446e7c9fbd959e8787c3608535cad2d7e7c26
88denooyer <88denooyer@cua.edu>
2019-02-18 19:04:08 -0500

    init

---
+++ getData.py
@@ -0,0 +1,87 @@
+import string
+import config
+import json
+import os
+
+from pprint import pprint
+
+with open('../randomDataGen/average.json') as data_file:
+    data = json.load(data_file)
+
+"""
+# need to figure out for loop to iterate through the Patient #
+# tried for i in range(1, 601) but that makes i an int
+# and i couldn't do ['Patient ' + i]
+
+# create function that takes input string of 'Patient 1'
+# read string p, a, t, i, e, n, t, (space), 1
+# separate patient and the digit
+# convert to int then increment then convert to string again
+# append increment string to Patient
+"""
+
+pain_list = []
+flex_list = []
+
+
+def get_sum(array):
+    sum_of_parts = 0
+    for i in range(0, len(array)):
+        sum_of_parts += array[i]
+    pprint(sum_of_parts)
+
+
+def define_flex_path(path, name):
+    with open('./DATA_SETS/' + path + '/' + name) as file_loc:
+        got_data = json.load(file_loc)
+        for i in range(0, 1000):
+            flex_list.append(got_data[i]['flex_index'])
+
+
+def define_pain_path(path, name):
+    with open('./DATA_SETS/' + path + '/' + name) as file_loc:
+        got_data = json.load(file_loc)
+    # prints entire file
+    # pprint(got_data)
+    # *returns* the first patient's flex index
+    a = []
+    for i in range(0, 1000):
+        # print(got_data[i]['pain_index'])
+        pain_list.append(got_data[i]['pain_index'])
+
+
+define_pain_path('GOOD_PATIENT', 'GPD1.json')
+define_flex_path('GOOD_PATIENT', 'GPD1.json')
+pprint('Pain Sum total')
+get_sum(pain_list)
+pprint('Flex Sum total')
+get_sum(flex_list)
+
+# pprint(pain_list)
+# pprint('END')
+# pprint(flex_list)
+
+
+"""
+pprint(define_file_path('GOOD_PATIENT', 'GPD1.json'))
+pprint(define_file_path('GOOD_PATIENT', 'GPD2.json'))
+pprint(define_file_path('GOOD_PATIENT', 'GPD3.json'))
+pprint(define_file_path('GOOD_PATIENT', 'GPD4.json'))
+pprint(define_file_path('GOOD_PATIENT', 'GPD5.json'))
+pprint(define_file_path('GOOD_PATIENT', 'GPD6.json'))
+pprint(define_file_path('GOOD_PATIENT', 'GPD7.json'))
+"""
+
+"""
+def readFile(filename):
+    filehandle = open(filename)
+    print(filehandle.read())
+    filehandle.close
+
+
+fileDir = os.path.dirname(os.path.relpath('__file__'))
+filename = os.path.join(fileDir, '../randomDataGen/average.json')
+readFile(filename)
+
+readFile(filename)
+"""
