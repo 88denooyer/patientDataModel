0c1a1ff7485ce65209e49f4d564b387e5a32d8d4
88denooyer <88denooyer@cua.edu>
2019-02-18 19:12:30 -0500

    changed comment todo's

--- getData.py
+++ getData.py
@@ -8,11 +8,11 @@ from pprint import pprint
 with open('../randomDataGen/average.json') as data_file:
     data = json.load(data_file)
 
-
 pain_list = []
 flex_list = []
 
 
+# simple function that will get sum of array items
 def get_sum(array):
     sum_of_parts = 0
     for i in range(0, len(array)):
@@ -20,6 +20,8 @@ def get_sum(array):
     pprint(sum_of_parts)
 
 
+# gets path for wanted file, finds the flex index part of the object
+# appends i - 1 data to array
 def define_flex_path(path, name):
     with open('./DATA_SETS/' + path + '/' + name) as file_loc:
         got_data = json.load(file_loc)
@@ -27,21 +29,24 @@ def define_flex_path(path, name):
             flex_list.append(got_data[i]['flex_index'])
 
 
+# does the same as above except with pain
+# see the comments for different testings
 def define_pain_path(path, name):
     with open('./DATA_SETS/' + path + '/' + name) as file_loc:
         got_data = json.load(file_loc)
     # prints entire file
     # pprint(got_data)
     # *returns* the first patient's flex index
-    a = []
     for i in range(0, 1000):
         # print(got_data[i]['pain_index'])
         pain_list.append(got_data[i]['pain_index'])
 
 
+# does all mapping and appending to the global array defined in function
 define_pain_path('GOOD_PATIENT', 'GPD1.json')
 define_flex_path('GOOD_PATIENT', 'GPD1.json')
 pprint('Pain Sum total')
+# gets the sums
 get_sum(pain_list)
 pprint('Flex Sum total')
 get_sum(flex_list)
@@ -51,6 +56,7 @@ get_sum(flex_list)
 # pprint(flex_list)
 
 
+# all garbage testing for trying different things
 """
 pprint(define_file_path('GOOD_PATIENT', 'GPD1.json'))
 pprint(define_file_path('GOOD_PATIENT', 'GPD2.json'))
